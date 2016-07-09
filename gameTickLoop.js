a56d799d5de2cae627213a308072f5d98ebfeac2
Craig Morton <CraigMorton92@gmail.com>
2016-07-09 06:40:50 +0100

    auto commit & push screeps script

--- gameTickLoop.js
+++ gameTickLoop.js
@@ -3,50 +3,60 @@ import roleUpgrader from "./creepRoles/upgrader.js";
 import roleBuilder from "./creepRoles/builder.js";
 
 const gameTickLoop = () => {
-  console.log("TICK in bundle from gameTickLoop.js");
 
-  const structs = Game.structures;
-  console.log("structures collection:",structs)
+// Always place this memory cleaning code at the very top of your main loop!
 
-  for (let i = 0; i < structs.length; i++){
-    console.log("struct #"+i,structs[i])
+for(var name in Memory.creeps) {
+  console.log("creep in memory:", name)
+  if(!Game.creeps[name]) {
+    delete Memory.creeps[name];
+    console.log('Clearing non-existing creep memory:', name);
+  }
+}
+console.log("TICK in bundle from gameTickLoop.js");
+
+const structs = Game.structures;
+console.log("structures collection:",structs)
+
+for (let i = 0; i < structs.length; i++){
+  console.log("struct #"+i,structs[i])
+}
+
+var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+console.log('Harvesters: ' + harvesters.length);
+
+if(harvesters.length < 2) {
+  var newName = Game.spawns.spawn.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+  console.log('Spawning new harvester: ' + newName);
+}
+
+var tower = Game.getObjectById(null);
+if(tower) {
+  var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+    filter: (structure) => structure.hits < structure.hitsMax
+  });
+  if(closestDamagedStructure) {
+    tower.repair(closestDamagedStructure);
   }
 
-  var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-  console.log('Harvesters: ' + harvesters.length);
-
-  if(harvesters.length < 2) {
-    var newName = Game.spawns.spawn.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
-    console.log('Spawning new harvester: ' + newName);
+  var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+  if(closestHostile) {
+    tower.attack(closestHostile);
   }
+}
 
-  var tower = Game.getObjectById(null);
-  if(tower) {
-    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-      filter: (structure) => structure.hits < structure.hitsMax
-    });
-    if(closestDamagedStructure) {
-      tower.repair(closestDamagedStructure);
-    }
-
-    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    if(closestHostile) {
-      tower.attack(closestHostile);
-    }
+for(var name in Game.creeps) {
+  var creep = Game.creeps[name];
+  if(creep.memory.role == 'harvester') {
+    roleHarvester.run(creep);
   }
-
-  for(var name in Game.creeps) {
-    var creep = Game.creeps[name];
-    if(creep.memory.role == 'harvester') {
-      roleHarvester.run(creep);
-    }
-    if(creep.memory.role == 'upgrader') {
-      roleUpgrader.run(creep);
-    }
-    if(creep.memory.role == 'builder') {
-      roleBuilder.run(creep);
-    }
+  if(creep.memory.role == 'upgrader') {
+    roleUpgrader.run(creep);
   }
+  if(creep.memory.role == 'builder') {
+    roleBuilder.run(creep);
+  }
+}
 }
 
 export {gameTickLoop as loop};
