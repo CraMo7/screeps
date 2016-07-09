a4bd9cae63a51b3e5750da12fea0ce2d88024356
Craig Morton <CraigMorton92@gmail.com>
2016-07-09 06:29:49 +0100

    auto commit & push screeps script

--- gameTickLoop.js
+++ gameTickLoop.js
@@ -11,12 +11,12 @@ const gameTickLoop = () => {
   for (let i = 0; i < structs.length; i++){
     console.log("struct #"+i,structs[i])
   }
-  
+
   var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
   console.log('Harvesters: ' + harvesters.length);
 
   if(harvesters.length < 2) {
-    var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+    var newName = Game.spawns.spawn.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
     console.log('Spawning new harvester: ' + newName);
   }
 
